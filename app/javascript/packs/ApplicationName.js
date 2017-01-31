e6393e209ea619bbfe1e6129afdc542f2bf16b57
joerodrig3 <joerodrig3@gmail.com>
2017-01-30 19:18:04 -0500

    Establishing a reusable pattern around img imports

--- app/javascript/packs/ApplicationName.js
+++ app/javascript/packs/ApplicationName.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {AVATAR_PLACEHOLDER} from "./lib/digestImgPath.js.erb";
+import {AVATAR_PLACEHOLDER, IC_LOADER} from "./lib/digestImgPath.js.erb";
 const demoStyles = {width: "25px", height: "25px"};
 
 const ApplicationName = (props) => (
   <span>
     <img src={AVATAR_PLACEHOLDER} style={demoStyles} />
     Demo Webpacker React
+    <img src={IC_LOADER} style={demoStyles} />
   </span>
 );
 
