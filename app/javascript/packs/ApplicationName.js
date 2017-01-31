e4dd9a187ffe2123d5a196c28d34211c1788c7b9
joerodrig3 <joerodrig3@gmail.com>
2017-01-30 19:14:59 -0500

    [broken] fix component syntax / imgName still breaks

--- app/javascript/packs/ApplicationName.js
+++ app/javascript/packs/ApplicationName.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom'
 import digestImgPath from "./lib/digestImgPath.js.erb";
 
 const ApplicationName = (props) => (
-  return (
-    <div>
+    <span>
       <img src={digestImgPath("avatar-placeholder.jpg")} style={demoStyles} />
-      <p>Demo Webpacker React</p>
-    </div>
-  )
+      Demo Webpacker React
+    </span>
 );
 
 export default ApplicationName;
