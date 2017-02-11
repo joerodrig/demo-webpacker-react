6d006ab3bff5bd5c79c66180895a21b28cbad615
Joseph <joerodrig3@gmail.com>
2017-02-10 22:38:53 -0500

    Setting up templates/view paths

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,13 @@ module DemoWebpackerReact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Support /app/view .rb classes and /app/templates files.
+    config.eager_load_paths << (Rails.root + "app/views/concerns").to_s
+    config.eager_load_paths << (Rails.root + "app/views").to_s
+    config.to_prepare do
+      ActionController::Base.send(:view_paths=, Rails.root.join("app", "templates"))
+      ActionController::API.send(:view_paths=, Rails.root.join("app", "templates"))
+    end
   end
 end
