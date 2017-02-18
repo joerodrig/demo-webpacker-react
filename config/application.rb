51f7ff0f4ef579319ea69b9eac8f5c2b30a87817
Joseph <joerodrig3@gmail.com>
2017-02-18 12:54:22 -0500

    precompile fonts

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module DemoWebpackerReact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Compile fonts
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
