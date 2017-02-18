b66993d4b221540f5305d29b5cf5fb9806eb94e5
Joseph <joerodrig3@gmail.com>
2017-02-18 12:48:10 -0500

    [complete] refactored remaining web packer code

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,17 +1,9 @@
 // Note: You must restart bin/webpack-watcher for changes to take effect
-
-var path    = require('path')
-var webpack = require('webpack')
-var merge   = require('webpack-merge')
-
-var config = require('./shared.js')
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const config = require("./shared.js");
 
 module.exports = merge(config, {
-  output: { filename: "[name]-[hash].js" },
-
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true
-    })
-  ]
-})
+  output: {filename: "[name]-[hash].js"},
+  plugins: [new webpack.LoaderOptionsPlugin({minimize: true})],
+});
